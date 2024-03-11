4a8a07bc4c2809ddfb6a495c2a6cda82aa87971f
Saiganesh001 <saiganesh_pabbisetti@srmap.edu.in>
2024-03-11 23:21:53 +0530

    first commit

---
+++ src/Follower.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Follower = ({id, avatar_url, login, html_url}) => {
+  return (
+    <article className='card'>
+      <img src={avatar_url} alt={login}/>
+      <h4>{login}</h4>
+      <a href={html_url} className='btn'>View Profile</a>
+    </article>
+  )
+}
+
+export default Follower
